425ce51d1b43fb0aea3d694aa9de7982dcaeed0f
Tony Ramírez <tonyram9906@outlook.com>
2022-01-22 17:06:52 -0600

    Added goal in the furthest tile

--- source/scenes/Main/WorldGenerator.gd
+++ source/scenes/Main/WorldGenerator.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-const TOTAL_NUMBER_OF_ROOMS = 8
-const START_ROOM_COUNT = 3
-const ROOM_INCREASE_PER_LEVEL = 2
-const EXIT_ROOM_IND = 9
-const START_ROOM_IND = 0
 
+const START_ROOM_COUNT = 2
+const ROOM_INCREASE_PER_LEVEL = 2
+
+const TOTAL_ROOMS_AVAILABLE = 8
 const CELL_SIZE = 14
 const ROOMS_SIZE = 7
-const WORLD_SIZE = 10
+
+var world_size = 10
 
 var roomDict = {}
 var world = []
@@ -17,35 +17,42 @@ var occupied_rooms = []
 
 func _ready():
 	randomize()
+	var level = 10
+	world_size += level
+	var total_rooms = START_ROOM_COUNT + level * ROOM_INCREASE_PER_LEVEL
 	#loading room templates
-	for room_num in (TOTAL_NUMBER_OF_ROOMS):
+	for room_num in (TOTAL_ROOMS_AVAILABLE):
 		roomDict[room_num] = load("res://scenes/Maps/room" + str(room_num) + ".tscn")
 	#var room_start = load("res://scenes/Maps/roomstart.tscn").instance()
 	#var room_finish = load("res://scenes/Maps/roomfinish.tscn").instance()
 	
 	#generate world grid
 		#0 means empty, 1 means spawnable, 2 means occupied, 3 means block
-	for i in WORLD_SIZE:
+	for i in world_size:
 		world.append([])
-		world[i].resize(WORLD_SIZE)
-		for j in WORLD_SIZE:
+		world[i].resize(world_size)
+		for j in world_size:
 			world[i][j] = 0
-
-	world[WORLD_SIZE/2 | 0][WORLD_SIZE/2 | 0] = 2
+	#occupy center tile
+	world[world_size/2 | 0][world_size/2 | 0] = 2
 	
-	for i in 25:
-		getPosibleRoomLocations(posible_rooms, 1)
+	#select rooms x number of times
+	for i in total_rooms:
+		getPosibleRoomLocations(posible_rooms, 1, 4)
 		selectNewRoom(posible_rooms)
+	#surround all rooms with walls
+	getPosibleRoomLocations(posible_rooms, 3, 3)
+	calcFurthestCell(posible_rooms)
+	#spawn rooms and walls
 	spawnRooms(roomDict)
-	spawnWalls()
 	
 
-func getPosibleRoomLocations(list, num):
-	for i in WORLD_SIZE:
-		for j in WORLD_SIZE:
+func getPosibleRoomLocations(list, num, limit):
+	for i in world_size:
+		for j in world_size:
 			if world[i][j] == 2:
 				# check if available tile already listed, if dont, add it
-				if i < WORLD_SIZE - 1:
+				if i < world_size - limit:
 					if world[i+1][j] != 2:
 						world[i+1][j] = num
 						if !list.has(Vector2(i+1,j)):
@@ -55,7 +62,7 @@ func getPosibleRoomLocations(list, num):
 						world[i-1][j] = num
 						if !list.has(Vector2(i-1,j)):
 							list.append(Vector2(i-1,j))
-				if j < WORLD_SIZE - 1:
+				if j < world_size - limit:
 					if world[i][j+1] != 2:
 						world[i][j+1] = num
 						if !list.has(Vector2(i,j+1)):
@@ -67,29 +74,53 @@ func getPosibleRoomLocations(list, num):
 							list.append(Vector2(i,j-1))
 
 func selectNewRoom(list):
-	#randomly select one room from available
+	#randomly select one room from posibles
 	var new_room_pos = list[randi() % list.size()]
 	list.erase(new_room_pos)
 	world[new_room_pos.x][new_room_pos.y] = 2
 	
 func spawnRooms(rooms):
-	for i in WORLD_SIZE:
-		for j in WORLD_SIZE:
+	var room_start = load("res://scenes/Maps/roomstart.tscn").instance()
+	var room_finish = load("res://scenes/Maps/roomfinish.tscn").instance()
+	for i in world_size:
+		for j in world_size:
 			if world[i][j] == 2:
-				if i == (WORLD_SIZE/2 | 0) and j == (WORLD_SIZE/2 | 0):
+				if i == (world_size/2 | 0) and j == (world_size/2 | 0):
 					#instance start room at center
-					var room_start = load("res://scenes/Maps/roomstart.tscn").instance()
-					var room_finish = load("res://scenes/Maps/roomfinish.tscn").instance()
 					add_child(room_start)
-					room_start.set_position(ROOMS_SIZE*CELL_SIZE*Vector2(WORLD_SIZE/2 | 0,WORLD_SIZE/2 | 0))
+					room_start.set_position(ROOMS_SIZE*CELL_SIZE*Vector2(world_size/2 | 0,world_size/2 | 0))
 				else:
+					#instance random hallway
 					var new_room = rooms[randi() % rooms.size()].instance()
 					add_child(new_room)
 					new_room.set_position(CELL_SIZE*ROOMS_SIZE*Vector2(i,j))
-	
-func spawnWalls():
-	getPosibleRoomLocations()
+			elif world[i][j] == 3:
+					#instance wall
+				var room_wall = load("res://scenes/Maps/roomwall.tscn").instance()
+				add_child(room_wall)
+				room_wall.set_position(CELL_SIZE*ROOMS_SIZE*Vector2(i,j))
+			elif world[i][j] == 4:
+				#instance exit
+				add_child(room_finish)
+				room_finish.set_position(CELL_SIZE*ROOMS_SIZE*Vector2(i,j))
 
+func calcFurthestCell(list):
+	var furthest_room = Vector2(0,0)
+	var furthest = 0
+	var distance = 0
+	for room in list:
+		distance = sqrt(pow((room.y - (world_size/2 | 0)),2) + pow((room.x - (world_size/2 | 0)),2))
+		if distance > furthest:
+			furthest = distance
+			furthest_room = room
+	#print(furthest_room)
+	world[furthest_room.x][furthest_room.y] = 4
+	
+	for n in range(-1,2,1):
+		for m in range(-1,2,1):
+			if world[furthest_room.x + n][furthest_room.y + m] == 0:
+				world[furthest_room.x + n][furthest_room.y + m] = 3
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
