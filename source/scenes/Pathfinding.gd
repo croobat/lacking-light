a9271bddde4dcc3689127cde74bc00ef76c3790a
RodoPZ <rpinedo2799@gmail.com>
2022-01-23 17:43:07 -0600

    Maps, IA and world generator integrated

--- source/scenes/Pathfinding.gd
+++ source/scenes/Pathfinding.gd
@@ -2,21 +2,17 @@ extends TileMap
 class_name AStar_Path
 
 onready var astar = AStar2D.new()
-onready var used_cells = get_used_cells_by_id(1)
-
 onready var Enemy = get_parent().get_node("Enemy")
 onready var Player = get_parent().get_node("Player")
+
+var new_floor= []
 var path = []
 
-func _ready():
-	_add_points()
-	_connect_points()
-
-func _add_points():
+func _add_points(used_cells):
 	for cell in used_cells:
 		astar.add_point(id(cell),cell,1.0)
 
-func _connect_points():
+func _connect_points(used_cells):
 	for cell in used_cells:
 		var neighbors = [Vector2(1,0), Vector2(-1,0), Vector2(0,1), Vector2(0,-1)]
 		for neighbor in neighbors:
