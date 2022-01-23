c0fdca7cf8a1ef68c7a2bedfec5b77bab8e7d85c
RodoPZ <rpinedo2799@gmail.com>
2022-01-22 23:32:26 -0600

    Functioning IA and maps2

--- source/scenes/Player/Player.gd
+++ source/scenes/Player/Player.gd
@@ -1,15 +1,20 @@
 extends Area2D
 
 signal move
+var count = 0
 onready var collider = get_node("RayCast2D")
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
-	print(get_parent().get_node("Floor").get_cell(self.position.x/8,self.position.y/8))
-	move()
-	
+	#print(get_parent().get_node("Floor").get_cell(self.position.x/8,self.position.y/8))
+	count = move(count)
+	print(count)
+	if count == 3:
+		emit_signal("move")
+		count = 0
+		
 func check_tile(posx,posy):
 	var tile = get_parent().get_node("Floor").get_cell((self.position.x+posx)/8,(self.position.y+posy)/8)
 	if tile == -1 or tile == 1:
@@ -17,18 +22,18 @@ func check_tile(posx,posy):
 	else:
 		return true
 
-func move():
+func move(count):
 	if Input.is_action_just_released("ui_up") and check_tile(0,-8):
 		self.position.y -= 8
-		emit_signal("move")
+		count+=1
 	if Input.is_action_just_released("ui_down") and check_tile(0,8):
 		self.position.y += 8
-		emit_signal("move")
+		count+=1
 	if Input.is_action_just_released("ui_right") and check_tile(8,0):
 		self.position.x += 8
-		emit_signal("move")
+		count+=1
 	if Input.is_action_just_released("ui_left") and check_tile(-8,0):
 		self.position.x -= 8
-		emit_signal("move")
-		
+		count+=1
+	return	count	
 
